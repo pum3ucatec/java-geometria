46d5879e31c921e3db8ae7cb017958e8f0826410
GastonQuelali <gaston_q@hotmail.com>
2024-10-28 21:29:38 -0400

    Add class Geometria and Rectagulo

--- DibujoRectangulo.java
+++ DibujoRectangulo.java
@@ -2,21 +2,28 @@ import javax.swing.*;
 import java.awt.*;
 
 public class DibujoRectangulo extends JPanel {
-    
+
+    public Rectangulo r1;
+
+     public DibujoRectangulo() {
+        this.r1 = new Rectangulo(new Point(300, 200), new Point(500, 300), Color.RED);
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         // Dibuja un rectángulo
         g.setColor(Color.BLUE); // Color del rectángulo
         g.fillRect(50, 50, 200, 100); // (x, y, ancho, alto)
+        this.r1.dibujar(g);
     }
 
     public static void main(String[] args) {
         JFrame ventana = new JFrame("Dibujo de Rectángulo");
         DibujoRectangulo panel = new DibujoRectangulo();
-        
+       
         ventana.add(panel);
-        ventana.setSize(300, 200);
+        ventana.setSize(800, 800);
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ventana.setVisible(true);
     }
---
+++ Geometria.java
@@ -0,0 +1,10 @@
+import java.awt.Graphics;
+
+public abstract class Geometria {
+
+    public abstract void dibujar(Graphics g);
+
+    public abstract double calcularArea();
+
+    public abstract double calcularPerimetro();
+}
